79434d3c7116e03d4cf49a49598a9ec342c99c35
cenkh <1250578320@qq.com>
2015-04-14 16:59:57 +0800

    can push?

---
+++ Selected(Button)/Selected(Button)Tests/Selected_Button_Tests.m
@@ -0,0 +1,40 @@
+//
+//  Selected_Button_Tests.m
+//  Selected(Button)Tests
+//
+//  Created by LJ on 15/4/14.
+//  Copyright (c) 2015年 广东道一信息科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Selected_Button_Tests : XCTestCase
+
+@end
+
+@implementation Selected_Button_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
