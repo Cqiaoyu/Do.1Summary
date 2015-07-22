0ba5cbdaa35e27a22669ead990710d39110ab711
cenkh <1250578320@qq.com>
2015-07-22 17:05:24 +0800

    动态方法解析应用

---
+++ DynamicDemo/DynamicDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DynamicDemo
+//
+//  Created by LJ on 15/7/22.
+//  Copyright (c) 2015年 广东道一信息科技有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
